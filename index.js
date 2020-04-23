fb5326e8c0a305709d2eec3e643fb2fff716cf9f
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-23 00:46:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ class Lunch {
 
 class Dinner {
   constructor(salad, soup, entree, desert) {
-    this.salad = food;
-    this.drink = drink;
+     this.salad = salad;
+     this.soup = soup;
+     this.entree = entree;
+     this._desert = desert;
   }
 }
 
